7ad6d3c3a4ad0d06e7401440d568fbb4f794ee78
aCasualGoon <acasualgoon@gmail.com>
2023-11-05 05:29:57 +0100

    Add camera position, clipping planes, and field of view

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,21 +1,34 @@
 #include "Camera.h"
 #include <iostream>
 #include <GL/glew.h>
+#include <glm/glm.hpp> 
+using namespace glm;
 
-Camera::Camera(EngineContext *context, Shader *shader)
+mat4 matrix_from_posrot(const vec3 position, const vec3 rotation)
 {
-    const GLfloat vertices[20] = {
-    //    X      Y     Z     U     V
-        -1.0f, -1.0f, 0.0f, 0.0f, 0.0f, // Bottom left corner
-         1.0f, -1.0f, 0.0f, 1.0f, 0.0f, // Bottom right corner
-         1.0f,  1.0f, 0.0f, 1.0f, 1.0f, // Top right corner
-        -1.0f,  1.0f, 0.0f, 0.0f, 1.0f  // Top left corner
-    };
-    const GLuint indices[6] = { 0, 1, 2, 2, 3, 0 };
+    float sinX = sin(rotation.x), cosX = cos(rotation.x),
+          sinY = sin(rotation.y), cosY = cos(rotation.y),
+          sinZ = sin(rotation.z), cosZ = cos(rotation.z);
+    return mat4(
+        cosY * cosZ                     , -cosY * sinZ                     ,  sinY       ,  0.0f,
+        sinX * sinY * cosZ + cosX * sinZ,  cosX * cosZ - sinX * sinY * sinZ, -sinX * cosY,  0.0f,
+        sinX * sinZ - cosX * sinY * cosZ,  cosX * sinY * sinZ + sinX * cosZ,  cosX * cosY,  0.0f,
+        position.x                      ,  position.y                      ,  position.z ,  1.0f
+    );
+}
 
-    this->context = context;
-    this->shader = shader;
+const GLfloat vertices[20] = {
+//    X      Y     Z     U     V
+    -1.0f, -1.0f, 0.0f, 0.0f, 0.0f, // Bottom left corner
+     1.0f, -1.0f, 0.0f, 1.0f, 0.0f, // Bottom right corner
+     1.0f,  1.0f, 0.0f, 1.0f, 1.0f, // Top right corner
+    -1.0f,  1.0f, 0.0f, 0.0f, 1.0f  // Top left corner
+};
+const GLuint indices[6] = { 0, 1, 2, 2, 3, 0 };
 
+// GLuint VAO, VBO, EBO;
+void init_quad_data(GLuint &VAO, GLuint &VBO, GLuint &EBO)
+{
     // Create and bind a Vertex Array Object
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
@@ -37,8 +50,27 @@ Camera::Camera(EngineContext *context, Shader *shader)
     // UV coordinates
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat),(GLvoid*)(3 * sizeof(GLfloat)));
     glEnableVertexAttribArray(1);
+}
 
-    // Activate the shader
+Camera::Camera(EngineContext *context, Shader *shader)
+{
+    this->context = context;
+    this->shader = shader;
+
+    this->transform = matrix_from_posrot(vec3(0.0f, 0.0f, 0.0f), vec3(0.0f,0.0f,0.0f));
+
+    this->view = mat4(
+        1.0f, 0.0f, 0.0f, 0.0f,
+        0.0f, 1.0f, 0.0f, 0.0f,
+        0.0f, 0.0f, 1.0f, 0.0f,
+        0.0f, 0.0f, 0.0f, 1.0f
+    );
+
+    this->clip = vec2(0.0f, 1000.0f);
+
+    this->fov = 90.0f;
+
+    init_quad_data(this->VAO, this->VBO, this->EBO);
     shader->use();
 }
 
@@ -63,3 +95,28 @@ void Camera::cleanup()
     glDeleteBuffers(1, &VBO);
     glDeleteVertexArrays(1, &VAO);
 }
+
+
+// getters & setters
+vec3 Camera::get_position()
+    { return vec3(this->transform[3][0], this->transform[3][1], this->transform[3][2]); }
+void Camera::set_position(vec3 position)
+    { this->transform[3][0] = position.x; this->transform[3][1] = position.y; this->transform[3][2] = position.z; }
+
+vec2 Camera::get_clip()
+    { return this->clip; }
+void Camera::set_clip(vec2 clip)
+    { this->clip = clip; }
+float Camera::get_clip_near()
+    { return this->clip.x; }
+void Camera::set_clip_near(float clip_near)
+    { this->clip.x = clip_near; }
+float Camera::get_clip_far()
+    { return this->clip.y; }
+void Camera::set_clip_far(float clip_far)
+    { this->clip.y = clip_far; }
+
+float Camera::get_fov()
+    { return this->fov; }
+void Camera::set_fov(float fov)
+    { this->fov = fov; }
--- src/Camera.h
+++ src/Camera.h
@@ -2,22 +2,60 @@
 #define _CAMERA_H
 
 #include <GL/glew.h>
+#include <glm/glm.hpp>
+using namespace glm;
 #include "EngineContext.h"
 #include "Shader.h"
 
+/**
+ * @brief The Camera class represents a camera in the 3D world.
+ * 
+ * It is responsible for rendering the scene from its point of view.
+ */
 class Camera {
 private:
     EngineContext *context;
     Shader *shader;
-    /*transform matrix*/
-    /*projection matrix*/
-    /*view matrix*/
+    mat4 transform;
+    mat4 view;
+    vec2 clip; /** x = near, y = far */
+    float fov;
     GLuint VAO, VBO, EBO;
 public:
+    /**
+     * @brief Constructs a new Camera object.
+     * @param context The engine context.
+     * @param shader The shader used for rendering the scene.
+     */
     Camera(EngineContext *context,Shader *shader);
 
+    /** @brief Gets the camera's position. @return The camera's position. */
+    vec3 get_position();
+    /** @brief Sets the camera's position. @param position The new position. */
+    void set_position(vec3 position);
+
+    /** @brief Gets the camera's clipping planes. @return The camera's clipping planes (near, far). */
+    vec2 get_clip();
+    /** @brief Sets the camera's clipping planes. @param clip The new clipping planes (near, far). */
+    void set_clip(vec2 clip);
+    /** @brief Gets the camera's near clipping plane. @return The camera's near clipping plane. */
+    float get_clip_near();
+    /** @brief Sets the camera's near clipping plane. @param clip_near The new near clipping plane. */
+    void set_clip_near(float clip_near);
+    /** @brief Gets the camera's far clipping plane. @return The camera's far clipping plane. */
+    float get_clip_far();
+    /** @brief Sets the camera's far clipping plane. @param clip_far The new far clipping plane. */
+    void set_clip_far(float clip_far);
+
+    /** @brief Gets the camera's field of view. @return The camera's field of view. */
+    float get_fov();
+    /** @brief Sets the camera's field of view. @param fov The new field of view. */
+    void set_fov(float fov);
+
+    /** @brief Renders the scene from the camera's point of view. */
     void render();
 
+    /** @brief Cleans up the camera resources. */
     void cleanup();
 
     ~Camera() {cleanup();}
